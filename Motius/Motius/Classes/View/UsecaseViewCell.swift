f19f062423c7fdb8812ef07389940f5d1ad01724
Lorenzo Donini <lorenzo.donini@tum.de>
2016-08-20 21:14:20 +0200

    Add custom cell view for Usecases tableview

---
+++ Motius/Motius/Classes/View/UsecaseViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UsecaseViewCell.swift
+//  Motius
+//
+//  Created by Lorenzo Donini on 20/08/16.
+//  Copyright © 2016 Motius GmbH. All rights reserved.
+//
+
+import UIKit
+
+class UsecaseViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
