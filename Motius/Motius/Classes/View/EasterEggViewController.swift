efcc8aee79cd103c010f8740bd8c22dbaf0bf303
Lorenzo Donini <lorenzo.donini@tum.de>
2016-08-21 13:03:33 +0200

    Implement EasterEggViewController logic
    Download random image when view appears
    Implement catch mechanism and alert
    Add image animation

--- Motius/Motius/Classes/View/EasterEggViewController.swift
+++ Motius/Motius/Classes/View/EasterEggViewController.swift
@@ -28,18 +28,74 @@ class EasterEggViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(animated: Bool) {
+        currentStatus = "Walking in tall grass..."
+        catchButton.hidden = true
+        //Choosing a random element out of the database
+        let randomChoice = Int(arc4random_uniform(UInt32(db.count)))
+        if let url = NSURL(string: "https://img.pokemondb.net/artwork/\(db[randomChoice]).jpg") {
+            //Download image asynchronously for randomly chosen element
+            apiManager.getImageFromUrl(url) { (data: NSData?, error: ErrorType?) in
+                if let data = data {
+                    self.downloadedImage = data
+                    self.currentStatus = "A Wild Pokémon has just appeared!"
+                } else if let error = error {
+                    self.downloadedImage = nil
+                    self.currentStatus = "\(error)"
+                } else {
+                    self.downloadedImage = nil
+                    self.currentStatus = "Unknown error occurred!"
+                }
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.refreshEasterEgg()
+                })
+            }
+        }
     }
-    */
-
+    
+    override func viewWillDisappear(animated: Bool) {
+        downloadedImage = nil
+        currentStatus = nil
+        apiManager.cancelLastImageTask()
+    }
+    
+    //MARK: Domain logic
+    private func refreshEasterEgg() {
+        statusLabel.text = currentStatus ?? ""
+        guard let downloadedImage = downloadedImage else {
+            pkImageView.image = nil
+            return
+        }
+        guard let image = UIImage(data: downloadedImage) else {
+            currentStatus = "Invalid data was downloaded!"
+            return
+        }
+        pkImageView.image = image
+        //Showing pokemon via animation
+        pkImageView.alpha = 0
+        UIView.animateWithDuration(3.0, animations: {
+            self.pkImageView.alpha = 1
+            }) { _ in
+                self.catchButton.hidden = false
+        }
+    }
+    
+    @IBAction func catchIt() {
+        //Creating controller for the dialog window
+        let alertController = UIAlertController(title: "Got it!", message: "", preferredStyle: .Alert)
+        //Creation ok action for the dialog
+        let okAction = UIAlertAction(title: "Cool", style: .Default, handler: { _ -> Void in
+            //Resetting everything
+            self.currentStatus = "Walking in tall grass..."
+            self.pkImageView.image = nil
+            self.downloadedImage = nil
+            self.catchButton.hidden = true
+        })
+        alertController.addAction(okAction)
+        //Showing the dialog
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
 }
